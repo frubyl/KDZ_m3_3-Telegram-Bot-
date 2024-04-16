744dcf51b27dca85b48e27dece2c2e2f789801d9
frubyl <100238501+frubyl@users.noreply.github.com>
2024-04-16 17:19:11 +0300

    Add files via upload

---
+++ KDZ_3_m3/Logger/Logger.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Logger
+{
+
+    public class Logger : ILogger
+    {
+        private readonly string _categoryName;
+        private readonly StreamWriter _logWriter;
+        public void Log<TState>(LogLevel logLevel,EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            // Записываем только логи инф. уровня или выше.
+            if (!IsEnabled(logLevel))
+            {
+                return;
+            }
+            var message = formatter(state, exception);
+            // Записываем информацию в файл.
+            _logWriter.WriteLine($"[{logLevel}] [{_categoryName}] {message}");
+            _logWriter.Flush();
+        }
+
+        public IDisposable BeginScope<TState>(TState state) => null;
+
+        public bool IsEnabled(LogLevel logLevel) => logLevel >= LogLevel.Information;
+
+        public Logger(string categoryName, StreamWriter logFileWriter)
+        {
+            _categoryName = categoryName;
+            _logWriter = logFileWriter;
+        }
+        public Logger() { }
+    }
+}
---
+++ KDZ_3_m3/Logger/LoggerProvider.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Logger
+{
+    public class LoggerProvider : ILoggerProvider
+    {
+        private readonly StreamWriter _logWriter;
+
+        public ILogger CreateLogger(string categoryName) => new Logger(categoryName, _logWriter);
+
+        public void Dispose() => _logWriter.Dispose();
+
+        public LoggerProvider(StreamWriter logWriter)
+        {
+            _logWriter = logWriter ?? throw new ArgumentNullException(nameof(logWriter));
+        }
+        public LoggerProvider() { }
+
+    }
+}
